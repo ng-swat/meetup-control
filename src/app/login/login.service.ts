9512cfecaa978c4a06e909e14460406be01102f4
galta <gal.tamir@gmail.com>
2017-04-27 17:49:27 +0300

    Login Module (+ Components)

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -4,17 +4,17 @@ import {LoginForm, PasswordRecoverForm, RegistrationForm} from './login.interfac
 @Injectable()
 export class LoginService {
 
-  static login(loginData: LoginForm) {
+  constructor() { }
+
+  login(loginData: LoginForm) {
     console.log('LoginService.login()', loginData);
   }
 
-  static registration(registrationData: RegistrationForm) {
+  registration(registrationData: RegistrationForm) {
     console.log('LoginService.registration()', registrationData);
   }
 
-  static passwordRecover(passwordRecoverData: PasswordRecoverForm) {
+  passwordRecover(passwordRecoverData: PasswordRecoverForm) {
     console.log('LoginService.passwordRecover()', passwordRecoverData);
   }
-
-  constructor() { }
 }
