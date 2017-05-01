70fce45681e5c193e3f7362fb0f65688fd497990
galta <gal.tamir@gmail.com>
2017-05-01 11:29:06 +0300

    auto redirect to login

--- src/app/login/login.router.ts
+++ src/app/login/login.router.ts
@@ -4,6 +4,7 @@ import {RegistrationComponent} from './registration/registration.component';
 import {PasswordRecoverComponent} from './password-recover/password-recover.component';
 
 export const loginRoutes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
   { path: 'registration', component: RegistrationComponent},
   { path: 'password-recover', component: PasswordRecoverComponent},
